ba8a07fab378a3af148b60bb885181fd45ffc721
Guilherme Vezula <guilherme.vezula@pluritech.com.br>
2023-05-26 16:47:43 -0300

    refact card stories

--- src/stories/Card.stories.ts
+++ src/stories/Card.stories.ts
@@ -1,5 +1,4 @@
 import type { Meta, StoryObj } from '@storybook/react';
-import { Subtitle } from '../components/Subtitle/Subtitle';
 import { Card } from '../components/Card/Card';
 
 const meta = {
@@ -13,9 +12,9 @@ type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
   args: {
-    buttonLabel: "Button Label",
-    subtitle: "Subtitle SM",
     heading: "Heading SM",
+    subtitle: "Subtitle SM",
     paragraph: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+    buttonLabel: "Button Label",
   },
 };
